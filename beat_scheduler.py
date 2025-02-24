80d22da40e59fa0c6e0d1713e72afd00821a1a94
Samer <sameradel789@gmail.com>
2025-02-24 22:46:34 +0200

    Schedule every 6 hours

--- beat_scheduler.py
+++ beat_scheduler.py
@@ -4,8 +4,8 @@ from celery_app import celery
 celery.conf.beat_schedule = {
     "run_scraper_every_6_hours": {
         "task": "celery_app.run_spider",  # ✅ Corrected task name
-        "schedule": crontab(minute='*/1',
-                    # hour="*/6"
+        "schedule": crontab(minute=0,
+                    hour="*/6"
                     ),  # ✅ Runs every 6 hours
         "args": ("cnnarabic",),  # ✅ Use correct spider name
     },
